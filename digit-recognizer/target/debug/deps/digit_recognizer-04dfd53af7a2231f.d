a7d1e0118b62ae7a060967833fea0cfbf28ec1e8
BlazejZuk <blazej.zukk@gmail.com>
2026-05-06 16:45:25 +0200

    create rust project

---
+++ digit-recognizer/target/debug/deps/digit_recognizer-04dfd53af7a2231f.d
@@ -0,0 +1,5 @@
+C:\Dev\Rust-Neural-Network\digit-recognizer\target\debug\deps\digit_recognizer-04dfd53af7a2231f.d: src\main.rs
+
+C:\Dev\Rust-Neural-Network\digit-recognizer\target\debug\deps\libdigit_recognizer-04dfd53af7a2231f.rmeta: src\main.rs
+
+src\main.rs:
---
+++ digit-recognizer/target/debug/deps/digit_recognizer-3b67f05c2de4f5ff.d
@@ -0,0 +1,5 @@
+C:\Dev\Rust-Neural-Network\digit-recognizer\target\debug\deps\digit_recognizer-3b67f05c2de4f5ff.d: src\main.rs
+
+C:\Dev\Rust-Neural-Network\digit-recognizer\target\debug\deps\libdigit_recognizer-3b67f05c2de4f5ff.rmeta: src\main.rs
+
+src\main.rs:
