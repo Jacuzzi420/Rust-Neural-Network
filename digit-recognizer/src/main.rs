059f853b526c11dcf763d329f45849aaa3f2b9e2
BlazejZuk <blazej.zukk@gmail.com>
2026-05-15 22:32:37 +0200

    training on entire dataset

--- digit-recognizer/src/main.rs
+++ digit-recognizer/src/main.rs
@@ -1,34 +1,47 @@
 mod nn;
 mod data;
-
+use std::time::Instant;
 fn main() {
-    // example network:
-    let mut exmpl_network = nn::mlp::Network::new(vec![4, 3, 3, 2]); // 4 -> 3 -> 3 -> 2 network
-    exmpl_network.set_initial_layer(vec![0.6, 0.0, 1.0, 1.0]);
-
-    println!("\n\nINITIAL NETWORK:\n\n{}", exmpl_network);
-
-    exmpl_network.feedforward();
-
-    println!("\n\nNETWORK AFTER FEED FORWARD:\n\n{}", exmpl_network);
-
-
-    let data = data::mnist::load_mnist(
+    let training_data = data::mnist::load_mnist(
         "mnist/train-images.idx3-ubyte",
         "mnist/train-labels.idx1-ubyte",
-    ).expect("Failed to load MNIST data");
+    ).expect("Failed to load MNIST training_data");
 
-    println!("Loaded {} training images", data.images.len());
-    println!("First image label: {}", data.labels[0]);
+    let testing_data = data::mnist::load_mnist(
+        "mnist/t10k-images.idx3-ubyte",
+        "mnist/t10k-labels.idx1-ubyte",
+    ).expect("Failed to load MNIST training_data");
+
+    println!("Loaded {} training images", training_data.images.len());
+    println!("First image label: {}", training_data.labels[0]);
 
     let mut network = nn::mlp::Network::new(vec![784, 16, 16, 10]);
 
-    for _ in 0..100 {
-        network.learn(data.images[0].clone(), data.labels[0] as usize, 0.1);
-    }
+    let epochs = 20;
 
-    println!("Output layer: {:?}", &network.layers.last().unwrap().activations);
-    println!("Prediction: {}", network.predict(data.images[0].clone()));
+    for e in 0..epochs {
+        println!("Epoch {} started", e);
+        let now = Instant::now();
+
+        for i in 0..training_data.images.len() {
+            network.learn(training_data.images[i].clone(), training_data.labels[i] as usize, 0.1);
+        }
+
+
+        let elapsed_time = now.elapsed();
+        println!("Epoch {} done in {} seconds", e, elapsed_time.as_secs());
+
+        let mut correct = 0;
+        let total = testing_data.images.len();
+        for i in 0..total {
+            let prediction = network.predict(testing_data.images[i].clone());
+
+            if prediction == testing_data.labels[i] {
+                correct += 1;
+            }
+        }
+        println!("Correct: {} Total: {} Accuracy: {}", correct, total, correct as f32 / total as f32);
+    }
 }
 
 
